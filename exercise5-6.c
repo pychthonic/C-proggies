7582610d9e31cc16484a3e66a9e7ab0d57e1fd2d
pychthonic <fridgewolf@protonmail.com>
2020-01-18 22:11:35 -0500

    2 more exercises. my_readv is almost finished but too tired to debug tonight

---
+++ exercise5-6.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+/* Exercise 5-6. This was easy. 
+ * 
+ * The created file goes from
+ * 
+ * Hello,
+ * 
+ * to
+ * 
+ * Hello,world
+ * 
+ * to
+ * 
+ * HELLO,world
+ * 
+ * to
+ * 
+ * Giddayworld
+ *
+ * Real interesting stuff.
+ *
+ * */
+
+
+int main(int argc, char *argv[]) {
+    errno = 0; 
+    int fd1 = open("/tmp/file.txt", O_RDWR | O_CREAT | O_TRUNC,
+                   S_IRUSR | S_IWUSR);
+    if (fd1 == -1) {
+        errExit("open /tmp/file.txt");
+    }
+    
+    errno = 0; 
+    int fd2 = dup(fd1);
+    if (fd2 == -1) {
+        errExit("dup(fd1)");
+        close(fd1);
+    } 
+     
+    errno = 0; 
+    int fd3 = open("/tmp/file.txt", O_RDWR | O_CREAT | O_TRUNC,
+                   S_IRUSR | S_IWUSR);
+    if (fd2 == -1) {
+        close(fd1);
+        close(fd2); 
+        errExit("open /tmp/file.txt");
+    }
+    
+    errno = 0;
+    int bytes_written = write(fd1, "Hello,", 6);
+    if (bytes_written == -1) {
+        close(fd1);
+        close(fd2);
+        close(fd3);
+        errExit("write");
+    }
+     
+    errno = 0;
+    bytes_written = write(fd2, "world", 6);
+    if (bytes_written == -1) {
+        close(fd1);
+        close(fd2);
+        close(fd3);
+        errExit("write");
+    }
+
+    lseek(fd2, 0, SEEK_SET);
+
+    errno = 0;
+    bytes_written = write(fd1, "HELLO,", 6);
+    if (bytes_written == -1) {
+        close(fd1);
+        close(fd2);
+        close(fd3);
+        errExit("write");
+    }
+ 
+    errno = 0;
+    bytes_written = write(fd3, "Gidday", 6);
+    if (bytes_written == -1) {
+        close(fd1);
+        close(fd2);
+        close(fd3);
+        errExit("write");
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ my_writev.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+/* My implemention of writev() function.
+ * */
+
+ssize_t my_writev(int fd, const struct iovec *iov, int iovcnt) {
+    int i;
+    int bytes_written;
+    for (i=0; i<iovcnt; i++) {
+        errno = 0; 
+        bytes_written = write(fd, iov[i].iov_base, iov[i].iov_len);
+        errno = 0; 
+        if (bytes_written != iov[i].iov_len) {
+            close(fd);
+            errExit("write to fd");
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+  
+    char str1[] = "First line of the file\n";
+    char str2[] = "Second line of the file\n";
+    int num1 = 77;
+    float num2 = 7.7; 
+
+    struct iovec iovector[4];
+
+    iovector[0].iov_base = str1;
+    iovector[0].iov_len = strlen(str1);
+    iovector[1].iov_base = str2;
+    iovector[1].iov_len = strlen(str2);
+    iovector[2].iov_base = &num1;
+    iovector[2].iov_len = sizeof(int);
+    iovector[3].iov_base = &num2;
+    iovector[3].iov_len = sizeof(float);
+
+    errno = 0;
+    int fptr = open("/dev/fd/1", O_WRONLY);
+    if (fptr == -1) {
+        errExit("open(stdout, O_WRONLY)");
+    }
+    
+    my_writev(fptr, &iovector[0], 4);     
+
+    errno = 0;
+    if (close(fptr) == -1) {
+        errExit("close(fptr)");
+    } 
+    
+    return EXIT_SUCCESS;
+}
