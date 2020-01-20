1a917f6ecd18c8ad2af315077f13fedddc9b3341
pychthonic <fridgewolf@protonmail.com>
2020-01-20 09:18:49 -0500

    added showenv.c and using_getenv.c

---
+++ showenv.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
+#define MAXBUFFERSIZE 8192
+
+/* This program opens /proc/<pid>/environ, reads its contents into a
+ * char pointer array, and prints them to stdout.
+ * */
+
+
+int main(int argc, char *argv[]) {
+    char pid_str[10];
+    pid_t proc_pid = getpid();
+    snprintf(pid_str, 10, "%d", proc_pid); 
+    
+    char pathname[40] = "/proc/";
+    strcat(pathname, pid_str);
+    strcat(pathname, "/environ");
+    
+    int fptr = open(pathname, O_RDONLY);
+    if (fptr == -1) {
+        errExit("open");
+    }
+
+    char readbuf[MAXBUFFERSIZE];
+   
+    int bytes_read = read(fptr, readbuf, MAXBUFFERSIZE);
+    if (bytes_read == -1 ) {
+        close(fptr);
+        errExit("read");
+    }
+
+    int envcount = 0;    
+    int i; 
+    for (i=0; i<bytes_read; i++) {
+        if (readbuf[i] == '\0') {
+           envcount++;
+        }
+    }
+
+    char *env_ptr[envcount];
+    
+    env_ptr[0] = &readbuf[0];
+
+    int j = 1; 
+    for (i=0; i<bytes_read; i++) {
+        if (readbuf[i] == '\0') {
+            env_ptr[j] = &readbuf[i+1];
+            j++;
+        }
+    }
+
+    for (i=0; i<envcount; i++) {
+        printf("%s\n", env_ptr[i]);
+    }
+
+    if (close(fptr) == -1) {
+        errExit("close");
+    } 
+    
+    return EXIT_SUCCESS;
+}
--- using_getenv.c
+++ using_getenv.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -7,10 +9,12 @@
 #include <unistd.h>
 #include <string.h>
 
+
 #define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 /* This program prints out some environment variables for your
- * entertainment.
+ * entertainment. It also prints out program_invocation_name
+ * and program_invocation_short_name.
  * */
 
 
@@ -19,6 +23,8 @@ int main(int argc, char *argv[]) {
     printf("HOME: %s\n", getenv("HOME"));
     printf("ROOT: %s\n", getenv("ROOT"));
     printf("TERM: %s\n", getenv("TERM"));
+    printf("program_invocation_name: %s\n", program_invocation_name);
+    printf("program_invocation_short_name: %s\n", program_invocation_short_name);
 
     return EXIT_SUCCESS;
 }
