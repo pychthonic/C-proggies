85841f26834c52e4ff3131b58fcc5501f4981ee7
pychthonic <fridgewolf@protonmail.com>
2020-01-26 11:24:55 -0500

    added find_env_var_addy.c

--- find_env_var_addy.c
+++ find_env_var_addy.c
@@ -25,21 +25,50 @@ extern char **environ;
  * if I can find out where the char pointer poitners are
  * pointing to.
  *
+ * And it turns out, setenv() uses the heap for the strings
+ * it makes char **environ point to. This makes sense, since
+ * if the space above the stack was going to be used, it
+ * would have to be allocated for the largest possible
+ * strings that can be held as environment variables, which
+ * isn't a good use of virtual memory space. However, my 
+ * next question is, how is that memory free'd at the end
+ * of the program?
+ *
  * */
 
 int main(int argc, char *argv[]) {
+    
+
+    printf("\nsbrk(0) = %p\n", sbrk(0));
 
     printf("Original environment variables:\n\n");
+
+
+    printf("\nsbrk(0) = %p\n", sbrk(0));
 
     char **ep;
     for (ep = environ; *ep != NULL; ep++) {
         printf("Virtual memory address: %p\n", *ep);
         puts(*ep);
-        printf("strlen(env var) = %ld.", strlen(*ep));
-        printf("Adding 1 for null character makes %ld.\n", strlen(*ep) + 1);
         printf("Next char pointer should be located at %p\n", *ep + strlen(*ep) + 1);
         printf("\n");
     }        
-  
+    
+    printf("\nsbrk(0) = %p\n", sbrk(0));
+    printf("\n\n#####################################################\n\n");
+    printf("\n\n### Changing 'HOME' environment variable... ###\n\n");
+    
+    setenv("HOME", "NEW_HOME", 1);
+    
+    printf("\nsbrk(0) = %p\n", sbrk(0));
+    for (ep = environ; *ep != NULL; ep++) {
+        printf("Virtual memory address: %p\n", *ep);
+        puts(*ep);
+        printf("Next char pointer should be located at %p\n", *ep + strlen(*ep) + 1);
+        printf("\n");
+    }        
+
+    printf("\nsbrk(0) = %p\n", sbrk(0));
+
     return EXIT_SUCCESS;
 }
